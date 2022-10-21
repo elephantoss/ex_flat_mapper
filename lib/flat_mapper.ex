a9188665b3ecd10ce710bb3879822b90ed54b6ec
Elephant OSS <spirit@elephantoss.io>
2022-10-21 12:23:21 -0700

    feat: flatten maps convenience function
    implement convenience function to flatten maps

--- lib/flat_mapper.ex
+++ lib/flat_mapper.ex
@@ -1,5 +1,20 @@
 defmodule FlatMapper do
   @moduledoc """
-  Documentation for `FlatMapper`.
+  FlatMapper is a utility library that provides flatten functions for flattening
+  maps and structs.
+
+  This is a convenience module so one does not have to mind the specific type being
+  flattened.
   """
+
+  alias FlatMapper.Map
+
+  @doc """
+  Flattens the given value using the provided options.
+
+  For more details, please check the documentation for the specific type.
+  """
+  def flatten(value, opts \\ [])
+  def flatten(value, []) when is_map(value), do: Map.flatten(value)
+  def flatten(value, opts) when is_map(value), do: Map.flatten(value, opts)
 end
--- test/flat_mapper_test.exs
+++ test/flat_mapper_test.exs
@@ -1,4 +1,23 @@
 defmodule FlatMapperTest do
+  @moduledoc false
   use ExUnit.Case
   doctest FlatMapper
+
+  test "flatten/2 for maps" do
+    input = %{
+      "a" => "foo",
+      "b" => %{
+        "bar" => "baz"
+      }
+    }
+
+    expected = %{
+      "a" => "foo",
+      "b.bar" => "baz"
+    }
+
+    result = FlatMapper.flatten(input)
+
+    assert expected == result
+  end
 end
